619b80a8af899fbc13b9fd57e8b4aabf11ed98a1
ralph-brynard <ralph@thebrynards.com>
2023-08-08 15:33:01 -0500

    committing workflow

--- .github/workflows/actions/ansible/entrypoint.sh
+++ .github/workflows/actions/ansible/entrypoint.sh
@@ -1,7 +1,9 @@
-# BASH
-
-Copy entrypoint.sh .github/actions/ansible
-
-# POWERSHELL
-
-Copy-Item entrypoint.sh .github/actions/ansible
+#!/bin/bash
+if [ ! -z "$ANSIBLE_VAULT_PASSWORD" ]
+then
+    echo $ANSIBLE_VAULT_PASSWORD >> .vault;
+    ansible-playbook $1 --vault-password-file .vault;
+    rm .vault
+else
+    ansible-playbook $1
+fi
