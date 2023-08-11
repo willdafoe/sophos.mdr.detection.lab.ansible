ed1185fb47bd99aca0650907d20ec348717ee254
ralph-brynard <ralph@thebrynards.com>
2023-08-10 19:26:09 -0500

    setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-az ad app create --display-name $1
+appId=$(az ad app create --display-name $1 | jq '.id')
+
+assigneeObjectId=$(az ad sp create --id $appId)
+
+subscriptionId=$(az account show | jq '.id')
+
+tenantId=$(az account show | jq '.homeTenantId')
