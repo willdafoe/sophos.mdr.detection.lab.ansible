f887531df43b1603c7f7e4619cef59b972380b87
ralph-brynard <ralph@thebrynards.com>
2023-08-18 21:22:28 -0500

    workflow

--- .github/scripts/getArtifact.sh
+++ .github/scripts/getArtifact.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-repo="${repo:-default ""}"
-token="${token:-default ""}"
-artifact_name="${artifact_name:-default ""}"
-output_directory="$1"
+repo="$1"
+token="$1"
+artifact_name="$1"
+#output_directory="$1"
 scriptname=$0
 
 function usage {
@@ -11,9 +11,9 @@ function usage {
     echo " "
     echo " usage: $scriptname --token string --repo string "
     echo " "
-    echo "      --token string                     The Github Personal Access Token to use when making the API call. "
+    echo "      --token string                      The Github Personal Access Token to use when making the API call. "
     echo "      --repo string                       The Github Repository in <OWNER>/<NAME> format"
-    echo "      --artifact_name string          The name of the artifact file"
+    echo "      --artifact_name string              The name of the artifact file"
     echo " "
 }
 
@@ -22,42 +22,21 @@ function die {
     exit 1
 }
 
-function makeApiCall(){
-    local url="$1"
-    local headers=("${@:2}")
-    local options="${@:2}"
-    local headers_args=()
-    for header in "${headers[@]}"; do
-        headers_args+=("-H" "$header")
-    done
-    local response=$(curl -s -L "${headers_args[@]}" $options "$url")
-    echo "$response"
-}
-
 function getMostRecentArtifact(){
-    base_url="https://api.github.com"
-    artifact_url="$base_url/repos/$repo/actions/artifacts"
-    github_headers=("Accept: application/vnd.github+json" "Authorization: Bearer $token" "X-GitHub-Api-Version: 2022-11-28")
-    response=$(makeApiCall "${github_headers[@]}" "$artifact_url")
+    baseUri="https://api.github.com"
+    artifactUri="$baseUri/repos/$repo/actions/artifacts"
 
+    response=$(curl -s -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $token" -H "X-GitHub-Api-Version: 2022-11-28" "$artifactUri")
+
+    echo "$response"
     if  [ -n "$response" ]; then
-        most_recent_artifact=$( echo "$response" | jq --arg name "$artifact_name" '.artifacts | map(select(.name == "'$artifact_name'")) | sort_by(.created_at) | reverse')
+        most_recent_artifact=$(echo "$response" | jq --arg name "$artifact_name" '.artifacts[] | map(select(.name == "'$artifact_name'")) | sort_by(.created_at) | reverse')
         archive_download_url=$(echo "$most_recent_artifact" | jq -r 'first(.[] | .archive_download_url)')
+        echo "Most recent artifact_download_url is: $archive_download_url"
     else
         echo "No artifacts were found."
         exit 0
     fi
-    if [[ $output_directory ]]; then
-        curl -L -H "Authorization: Bearer $token"  "$archive_download_url" -o "$output_directory/$artifact_name.zip"
-        unzip "$artifact_name.zip"
-        rm -rf "$output_directory/$artifact_name.zip"
-    elif [[ -z $output_directory ]]; then
-        curl -L -H "Authorization: Bearer $token"  "$archive_download_url" -o "$(pwd)/$artifact_name.zip"
-        unzip "$artifact_name.zip"
-        rm -rf "$artifact_name.zip"
-    else
-        :
-    fi    
 }
 
 if [[ -z $token ]]; then
@@ -83,4 +62,5 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-getMostRecentArtifact
+artifact=$(getMostRecentArtifact)
+echo "$artifact"
---
+++ .github/scripts/script.py
@@ -0,0 +1,72 @@
+import time
+import json
+import os
+import urllib3
+
+TOKEN = os.environ["INPUT_TOKEN"]
+ARTIFACT_NAME = os.environ["INPUT_ARTIFACT_NAME"]
+NAME = os.environ["INPUT_RENAME"] or ARTIFACT_NAME
+REPO = os.getenv("INPUT_REPO") or os.getenv("GITHUB_REPOSITORY")
+WAIT_SECONDS = int(os.getenv("INPUT_WAIT_SECONDS") or "60")
+WAIT_SLEEP = 0.5
+GITHUB_OUTPUT = os.environ["GITHUB_OUTPUT"]
+
+artifacts_url = f"https://api.github.com/repos/{REPO}/actions/artifacts"
+headers = {
+    "Authorization": f"token {TOKEN}",
+    "User-Agent": "Python",
+}
+
+http = urllib3.PoolManager()
+
+
+def get_artifact(name):
+    print(f"Download `{name}` from: {artifacts_url}")
+
+    t_started = time.time()
+    waiting = True
+    etag = None
+
+    while waiting:
+        if etag:
+            resp = http.request(
+                "GET",
+                artifacts_url,
+                headers={**headers, "If-None-Match": etag},
+                fields={"name": ARTIFACT_NAME},
+            )
+        else:
+            resp = http.request(
+                "GET", artifacts_url, headers=headers, fields={"name": ARTIFACT_NAME}
+            )
+            etag = resp.headers.get("etag")
+
+        if resp.status == 200:
+            data = json.loads(resp.data.decode("utf-8"))
+            return data["artifacts"][0] if data["artifacts"] else None
+
+        waiting = time.time() - t_started < WAIT_SECONDS
+        time.sleep(1)
+        print("Waiting...", etag, resp.status)
+
+
+def set_output(name, value):
+    with open(GITHUB_OUTPUT, "a") as github_output:
+        github_output.write(f"{name}={value}\n")
+
+
+def download_artifact(name, new_name):
+    artifact = get_artifact(name)
+    if artifact is None:
+        set_output("error", f"Artifact not found: {name}")
+        exit(1)
+
+    r = http.request("GET", artifact["archive_download_url"], headers=headers)
+    with open(new_name, "wb") as f:
+        f.write(r.data)
+        set_output("success", "Artifact downloaded: {}".format(artifact["name"]))
+        set_output("commit", artifact["workflow_run"]["head_sha"])
+
+
+if __name__ == "__main__":
+    download_artifact(ARTIFACT_NAME, NAME)
